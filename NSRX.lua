134b6a7b5e00695c76199f599aea4c2702989076
Tanner Rogalsky <the.t.dawg@gmail.com>
2011-06-14 15:24:23 -0400

    Final commit... for now.

--- NSRX.lua
+++ NSRX.lua
@@ -20,16 +20,20 @@ function Card(speed, lift)
 
 	local liftCards = {"Music box: Puts all hammer bros. in play to sleep.", "-2 ton weight: We all know that all objects fall up at the same rate, regardless of mass.",
 		"My beautiful, my beautiful ballooooooon: Up and away.", "Cameron Winters: If only I could have remained...", "Spanish flea by the Tijuana Brass only in reverse: It is as if God Himself is speed dating.",
-		"Just another fucking balloon: Ugh.", "Boxers, right out of the freezer: Like an ice cold beer for your testicles.", "Snowglobe: Xanaduuuuu...", "Air screw: Da Vinci was so far behind him time.",
+		"Just another fucking balloon: Ugh.", "Boxers, right out of the freezer: Like an ice cold beer for your testicles.", "Snowglobe: Xanaduuuuu...", "Air screw: Da Vinci was so far behind his time.",
 		"Balloon: one, blue.", "New York style hotdog kiosk: Mustard, relish?  Yo!  Fuggeddabouddit.", "Hot-Air Balloon: Around the world in 80 days.",
 		"Roswellian Airfoil: This technology is strangely familiar (anus-scratching noise).", "One forgotten vector: Such a magnificent magnitude gone to waste.",
 		"Matthew Lesko's Inquisition Coat: Tell bill collectors to go shove it.", "Buouyant Carrot: Vitamin Up."}
+
+	local specialCards = {"A Space Odyssey: Daisy, daisy.  Give me your answer... do...", "HAL: Thank you for a very enjoyable game."}
 
 	local this = {}
 	this.speed = speed
 	this.lift = lift
 	if lift == 0 and speed == 0 then
 		this.name = zeroCards[math.random(#zeroCards)]
+	elseif lift == 2001 or speed == 2001 then
+		this.name = specialCards[math.random(#specialCards)]
 	elseif lift >= speed then
 		this.name = liftCards[math.random(#liftCards)]
 	elseif lift < speed then
@@ -97,9 +101,12 @@ function NSRX:parseCommand(nick, msg)
 			end
 		end
 		self.inGame = true
+		self.pointsToWin = tonumber(msg:match('^!start (%d-)%s*$')) or 7;
+		privmsg (self.channel, self.colours.red.."Playing to ".. self.pointsToWin..".")
 		privmsg (self.channel, self:newTurn())
 	elseif msg:find("^!play") and nick == self.currentPlayer.name then
-		self.dealtTo = self.players[msg:match('^!play (.-)%s*$')]
+		local n = msg:match('^!play (.-)%s*$') or self.currentPlayer.name
+		self.dealtTo = self.players[n]
 		if self.dealtTo then
 			if #self.dealtTo.cards < 3 then
 				self:dealCard()
@@ -108,6 +115,8 @@ function NSRX:parseCommand(nick, msg)
 				privmsg (self.channel, self.colours.red..self.dealtTo.name.." already has ".. #self.dealtTo.cards.. " cards.")
 				privmsg (self.channel, self.colours.green..self.dealtTo.name..": Choose a card [X//Y] to remove:"..cardsToString(self.dealtTo))
 			end
+		else
+			privmsg (self.channel, self.colours.red..n.." is not a valid player name. (Did you forget to capitalize?)")
 		end
 	elseif self.players[msg:match('^!(.-)%s*$')] then
 		privmsg (self.channel, self.colours.green..tostring(self.players[msg:match('^!(.-)%s*$')]))
@@ -115,7 +124,7 @@ function NSRX:parseCommand(nick, msg)
 		for _,player in pairs(self.players) do
 			privmsg (self.channel, self.colours.green..tostring(player))
 		end
-	elseif self.dealtTo ~= nil and nick == self.dealtTo.name then
+	elseif self.dealtTo ~= nil and nick == self.dealtTo.name and self.inGame then
 		local speed, lift = msg:match("(%d)//(%d)")
 		if tonumber(speed) ~= nil and tonumber(lift) ~= nil then
 			local toRemove = Card(tonumber(speed), tonumber(lift))
@@ -133,7 +142,7 @@ function NSRX:parseCommand(nick, msg)
 			privmsg (self.channel, self:newTurn())
 		end
 	elseif msg:find("^!current") and self.inGame then
-		privmsg (self.channel, self.colours.red.."Current card: "..tostring(self.currentCard).. " - Current player: ".. self.currentPlayer.name..".")
+		privmsg (self.channel, self.colours.red.."Current card: "..tostring(self.currentCard).. " - Current player: ".. self.currentPlayer.name.." - Playing to "..self.pointsToWin..".")
 	elseif msg:find("^!end") and self.inGame then
 		self.currentPlayer = nil
 		self.inGame = false
@@ -179,11 +188,39 @@ end
 function NSRX:newTurn()
 	if self.inGame then
 		self.dealtTo = nil
-		self.currentCard = Card(rand(6) - 1, rand(6) - 1)
+		self.currentCard = Card(self.getNumber(), self.getNumber())
 		self.currentPlayer = self.currentPlayer.next
 		return self.colours.red.."Current player: ".. self.currentPlayer.name.. ": Current card: "..tostring(self.currentCard).."."
 	end
 end
 
+function NSRX:getNumber()
+	local r = rand(30)
+	if r == 1 then
+		return 2001
+	end
+
+	r = rand(20)
+	if r <= 5 then
+		return 0
+	elseif r <= 7 then
+		return 1
+	elseif r <= 12 then
+		return 2
+	elseif r <= 15 then
+		return 3
+	elseif r <= 16 then
+		return 4
+	elseif r <= 18 then
+		return 5
+	elseif r <= 20 then
+		return 6
+	end
+	return nil
+end
+
 
 return NSRX
+
+--~ <blue_tetris> Okay, let's make 2 most likely; 3 the second most likely; 1 and 4 the third msot likely; 5 and 0 the next; and then 6.
+--~ <blue_tetris> Wanna frequency chart?  0 - 3 in 20.  1 - 2 in 20.  2 - 6 in 20.  3 - 4 in 20.  4 - 2 in 20.  5 - 2 in 20.  6 - 1 in 20.
